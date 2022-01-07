f2f939b963d15529890f5ba83a327b4ef27b5e09
dgjinsu <rlawlstn3206@naver.com>
2022-01-07 14:32:43 +0900

    연습

---
+++ 1874.py
@@ -0,0 +1,23 @@
+import sys
+n = int(sys.stdin.readline())
+list1 = []
+result = []
+plus_minus = []
+count = 1
+compare = []
+for i in range(n):
+	num = int(sys.stdin.readline())
+	compare.append(num)
+	while count <= num:
+		list1.append(count)
+		plus_minus.append("+")
+		count += 1
+		result.append(list1.pop())
+		plus_minus.append("-")
+
+if compare == result:
+	for i in plus_minus:
+		print(i)
+else :
+	print("NO")
+
